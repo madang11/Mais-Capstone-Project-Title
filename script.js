f752849c2f8e146115b2eb7dd1205ae7f7589083
madang11 <46465661+madang11@users.noreply.github.com>
2020-02-24 16:58:21 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,44 @@
+
+$(".feetBox").click(function(){
+    $(".textBoxes").text("Some of the obstacles i have encountered are mostly around school. I have been trying to study materials outside of school to be ahead in class. Some obstacles I am facing right now are "); 
+    $(".feet").show(); 
+    $(".hands").hide(); 
+    $(".brain").hide(); 
+    $(".heart").hide(); 
+});
+$(".feetBox2").click(function(){
+    $(".textBoxes").text("Some of the obstacles i have encountered are mostly around school. I have been trying to study materials outside of school to be ahead in class.  "); 
+    $(".feet").show(); 
+    $(".hands").hide(); 
+    $(".brain").hide(); 
+    $(".heart").hide(); 
+});
+$(".handBox").click(function(){
+    $(".textBoxes").text("I have been participating golf and badminton at school. My mentors have helped me develop my writing skills and my people skills. I wanna be a computer engineer in the future and study in a four year college "); 
+    $(".feet").hide(); 
+    $(".hands").show(); 
+    $(".brain").hide(); 
+    $(".heart").hide(); 
+});
+$(".handBox2").click(function(){
+    $(".textBoxes").text("I have been participating golf and badminton at school. My mentors have helped me develop my writing skills and my people skills. I wanna be a computer engineer in the future and study in a four year college "); 
+    $(".feet").hide(); 
+    $(".hands").show(); 
+    $(".brain").hide(); 
+    $(".heart").hide(); 
+});
+$(".brainBox").click(function(){
+    $(".textBoxes").text("The things I have learn this year is mostly about myself and others. Even though I definitely learned a lot in math and english, the activities we do during pod session taught me about my personality and tendencies such as the MBTI and about other people (Conflict styles). I want to learn more myself and some of my interests next year (coding, drawing ect)"); 
+    $(".feet").hide(); 
+    $(".hands").hide(); 
+    $(".brain").show(); 
+    $(".heart").hide(); 
+});
+$(".heartBox").click(function(){
+    $(".textBoxes").text("I love to draw and observe my surroundings. I wanna practice my drawing skills and share my arr with my community. My mentors have helped me with my college applications and have helped me get into Boston University. "); 
+    $(".feet").hide(); 
+    $(".hands").hide(); 
+    $(".brain").hide(); 
+    $(".heart").show(); 
+});
+
